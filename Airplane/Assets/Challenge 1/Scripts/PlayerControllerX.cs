c850d608b9299656e57e099e23576a1144584323
MARS <61511540+MARSMPEI@users.noreply.github.com>
2022-10-13 00:46:46 +0300

    auto_flying

--- Airplane/Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Airplane/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
-    public float speed = 1f;
+    public float speed = 10f;
     public float rotationSpeed;
     public float verticalInput;
     public float horizontalInput;
@@ -22,7 +22,7 @@ public class PlayerControllerX : MonoBehaviour
         verticalInput = Input.GetAxis("Vertical");
         horizontalInput = Input.GetAxis("Horizontal");
         // move the plane forward at a constant rate
-        transform.Translate(Vector3.forward * speed * Time.deltaTime* verticalInput);
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
 
         // tilt the plane up/down based on up/down arrow keys
         transform.Rotate(Vector3.right, rotationSpeed * Time.deltaTime* horizontalInput);
